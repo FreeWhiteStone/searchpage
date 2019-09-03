2a9418200ef04b8dff58d6741d631a23812dc1c9
Özgür Aktaş <33002420+FreeWhiteStone@users.noreply.github.com>
2019-09-03 19:17:33 +0300

    edit
    edit

---
+++ build/precache-manifest.7ae3ed9dbbc4fe62b70c786e8ab91fa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5422001774550e3e910418b03165a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068b23c2d925fffc25cc",
+    "url": "/static/css/main.7a8521de.chunk.css"
+  },
+  {
+    "revision": "9ce0623b48eac0e2bc29",
+    "url": "/static/js/2.4fa6f745.chunk.js"
+  },
+  {
+    "revision": "068b23c2d925fffc25cc",
+    "url": "/static/js/main.387c841e.chunk.js"
+  },
+  {
+    "revision": "5ceb89ddf2a8c570af40",
+    "url": "/static/js/runtime~main.c35ae77e.js"
+  }
+]);
